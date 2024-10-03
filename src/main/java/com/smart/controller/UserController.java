4deb4c3c3ccf66bc0cf55a76d827c33349d570a4
Pankaj726655 <pp26421@gmail.com>
2024-10-03 18:05:10 +0530

    Adding add contact page, process contact.

--- src/main/java/com/smart/controller/UserController.java
+++ src/main/java/com/smart/controller/UserController.java
@@ -1,11 +1,14 @@
 package com.smart.controller;
 
+import com.smart.model.Contact;
 import com.smart.model.User;
 import com.smart.repo.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.security.Principal;
@@ -17,12 +20,36 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
+    @ModelAttribute
+    public void addCommonData(Model model, Principal principal){
+        model.addAttribute("user", userRepository.findByEmail(principal.getName()));
+    }
+
+    // dashboard home
     @RequestMapping("/index")
     public String dashboard(Model model, Principal principal){
-        User user = userRepository.findByEmail(principal.getName());
-        model.addAttribute("user", user);
+        model.addAttribute("title", "User Dashboard");
         return "normal/user_dashboard";
     }
+
+    // Open add contact form
+    @GetMapping("/add-contact")
+    public String openAddContactForm(Model model){
+        model.addAttribute("title", "Add Contact");
+        model.addAttribute("contact", new Contact());
+    return "normal/add_contact_form";
+    }
+
+    @PostMapping("/process-contact")
+    public String processContact(@ModelAttribute Contact contact, Principal principal){
+        User user = userRepository.findByEmail(principal.getName());
+        contact.setUser(user);
+        user.getContacts().add(contact);
+        userRepository.save(user);
+        System.out.println("added");
+        return "normal/add_contact_form";
+    }
+
 
 
 }
